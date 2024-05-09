4a22a376626ec1212e4c5938caf754066ef427b2
Fabian Mardones <fabianmardones94@gmail.com>
2024-05-09 01:43:27 -0400

    modo oscuro activado

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -10,25 +10,25 @@ import Hamburguer from "../Button";
 import Button from "../Button";
 
 const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const { state, dispatch } = useGlobalReduceState();
 
-  const handleCloseMenu = () => {
-    setIsOpen(false);
-  };
+  console.log(state.darkMode);
 
   console.log(state);
 
   return (
-    <nav className="navbar"> 
+    <nav className={`navbar ${state.darkMode && 'dark'}`}> 
       <div className="shadow-md w-full fixed top-0 left-0">
-        <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-5">
+        <div className={`md:flex items-center justify-between bg-white py-4 md:px-10 px-5 ${state.darkMode && 'dark'}`}>
           <div className="flex items-center">
             <NavLink
               to={"/"}
               className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
             >
-              <img className="w-15 h-16" src="/public/img/logo.png" />
+              {
+                state.darkMode ? <img className={`${state.darkMode && 'dark'}w-15 h-16 logo2`} src="/public/img/image.png" /> : <img className={`${state.darkMode && 'dark'} w-15 h-16 logo1`} src="/public/img/logo.png" /> 
+              }
+
             </NavLink>
             <form className="hidden md:flex ml-0">
               <input
@@ -44,14 +44,14 @@ const Nav = () => {
           </div>
 
           <div className="flex items-center ml-8">
-            <NavLink to={routes.registrarse} onClick={handleCloseMenu}>
+            <NavLink to={routes.registrarse}>
             <Button>
-                Get started
+                Signup
             </Button>
             </NavLink>
-            <NavLink to={routes.inicioSesion} onClick={handleCloseMenu}>
+            <NavLink to={routes.inicioSesion}>
             <Button>
-                Get started
+                Login
             </Button>
             </NavLink>
             <button className="mr-5 hover:text-gray-900 inline-flex items-center border-0 py-3 px-4 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 shadow-md font-semibold">
