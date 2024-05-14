cf450b0b17732eabe7ed3cc9cc2c723beb60bda4
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-05-14 01:10:57 -0500

    update
    se agrega componente  Products, se color de fondo de botones , se renderizan imagenes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = (props) => {
   return (
-    <button className="bg-white text-blue-500 font-bold border rounded-lg md:shadow-lg font-poppins py-2 px-6 rounded md:ml-1 mr-5 hover:bg-indigo-400 hover:text-white">
+    <button className="bg-gray-300 text-gray-700 font-bold border rounded-lg md:shadow-lg font-poppins py-2 px-6 rounded md:ml-1 mr-5 hover:bg-indigo-400 hover:text-white">
       {props.children}
     </button>
   );
