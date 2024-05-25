0e6ca8cbeee7c5a294f3dae7ff33d448229d81ad
Sebasv0120 <brayanvarela1234@gmail.com>
2024-05-24 21:03:11 -0500

    usuarioPerfil
    Se creo un componente llamado UsuarioRegistro

--- src/components/CreateProduct/CreateProduct.jsx
+++ src/components/CreateProduct/CreateProduct.jsx
@@ -1,6 +1,6 @@
 import { useGlobalReduceState } from "../utils/GlobalContextReducer";
 import React, { useState } from 'react';
-import './CreateProduct.css'
+import './CreateProduct.css';
 
 const CreateProduct = () => {
   const { state, dispatch } = useGlobalReduceState();
@@ -37,62 +37,63 @@ const CreateProduct = () => {
   };
 
   return (
-    <div className="create-product-container">
-      <h2 className="create-product-heading">Crear Nuevo Producto</h2>
-      <form onSubmit={handleSubmit} className="create-product-form">
-      <input
-        type="text"
-        name="name"
-        placeholder="Nombre"
-        value={newProduct.name}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="number"
-        name="price"
-        placeholder="Precio"
-        value={newProduct.price}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="number"
-        name="stock"
-        placeholder="Stock"
-        value={newProduct.stock}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="description"
-        placeholder="Descripción"
-        value={newProduct.description}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="category"
-        placeholder="Categoría"
-        value={newProduct.category}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="img"
-        placeholder="URL de la imagen"
-        value={newProduct.img}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit" className="btn-submit">Añadir Producto</button>
-    </form>
-
-    </div>
-    
+    <>
+      <div className="create-product-container">
+        <h2 className="create-product-heading">Crear Nuevo Producto</h2>
+        <form onSubmit={handleSubmit} className="create-product-form">
+          <input
+            type="text"
+            name="name"
+            placeholder="Nombre"
+            value={newProduct.name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="number"
+            name="price"
+            placeholder="Precio"
+            value={newProduct.price}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="number"
+            name="stock"
+            placeholder="Stock"
+            value={newProduct.stock}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Descripción"
+            value={newProduct.description}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="Categoría"
+            value={newProduct.category}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="img"
+            placeholder="URL de la imagen"
+            value={newProduct.img}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" className="btn-submit">Añadir Producto</button>
+        </form>
+      </div>
+      <div className="unavailable-message">El formulario no está disponible</div>
+    </>
   );
 };
 
---
+++ src/components/Profile/UsuarioRegistrado.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './UsuarioRegistrado.css'; // Importa el archivo CSS del perfil de usuario
+
+const UsuarioRegistrado = () => {
+  // Aquí puedes agregar la lógica necesaria para obtener los datos del usuario, si es necesario
+
+  return (
+    <div className='contenedorPadre'>
+    <div className="perfil-container">
+      <h1 className="perfil-titulo">Perfil de Usuario</h1>
+      <div className="perfil-info">
+        <div className="campo-info">
+          <span className="campo-titulo">Nombre:</span>
+          <span className="campo-valor">Nombre del Usuario</span>
+        </div>
+        <div className="campo-info">
+          <span className="campo-titulo">Correo electrónico:</span>
+          <span className="campo-valor">usuario@example.com</span>
+        </div>
+        {/* Puedes agregar más campos de información del usuario según tus necesidades */}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default UsuarioRegistrado;
