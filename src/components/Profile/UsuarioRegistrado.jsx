122fbf7a660a9d6783fe9f7c6bcb82c223ae8fc0
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-05-27 20:49:49 -0500

    add design
    css style and material ui card added to user profile

--- src/components/Profile/UsuarioRegistrado.jsx
+++ src/components/Profile/UsuarioRegistrado.jsx
@@ -1,40 +1,74 @@
-import React from 'react';
-import './UsuarioRegistrado.css'; // Importa el archivo CSS del perfil de usuario
-import { useAuthContext } from '../utils/AuthContext';
-import { Button } from '@mui/material';
+import React from "react";
+import "./UsuarioRegistrado.css"; // Importa el archivo CSS del perfil de usuario
+import { useAuthContext } from "../utils/AuthContext";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Button,
+  Typography,
+} from "@mui/material";
 
 const UsuarioRegistrado = () => {
-  
-  const { user,logout, loading }=useAuthContext();
+  const { user, logout, loading } = useAuthContext();
 
-  const handleLogout = async() =>{
+  const handleLogout = async () => {
     await logout();
-    console.log('cerro sesion')
+    console.log("Cerró sesión");
   };
 
-  if (loading) return <h1>loading</h1>
+  if (loading) return <h1>loading</h1>;
 
+  // Obtener las iniciales del nombre y apellido
+  const getInitials = (name) => {
+    const initials = name
+      .split(" ")
+      .map((n) => n[0])
+      .join("");
+    return initials.toUpperCase();
+  };
 
   return (
-    <div className='contenedorPadre'>
-    <div className="perfil-container">
-      <h1 className="perfil-titulo">Perfil de Usuario</h1>
-      <div className="perfil-info">
-        <div className="campo-info">
-          <span className="campo-titulo">Nombre:</span>
-          <span className="campo-valor">{user.displayName}</span>
-        </div>
-        <div className="campo-info">
-          <span className="campo-titulo">Correo electrónico:</span>
-          <span className="campo-valor">{user.email}</span>
-        </div>
-        <div className="campo-info">
-          <span className="campo-titulo">Contraseña:</span>
-          <span className="campo-valor">*********</span>
-        </div>
-        <Button onClick={handleLogout}>Cerrar sesión</Button>
-
-      </div>
+    <div className="contenedorPadre">
+      <Card className="perfil-tarjeta">
+        <CardHeader
+          avatar={
+            <Avatar className="avatar">{getInitials(user.displayName)}</Avatar>
+          }
+          title={
+            <Typography variant="h5" className="perfil-titulo">
+              {user.displayName}
+            </Typography>
+          }
+        />
+        <CardContent className="perfil-info">
+          <div className="campo-info">
+            <Typography variant="body2" className="campo-titulo">
+              Correo electrónico:
+            </Typography>
+            <Typography variant="body2" className="campo-valor">
+              {user.email}
+            </Typography>
+          </div>
+          <div className="campo-info">
+            <Typography variant="body2" className="campo-titulo">
+              Contraseña:
+            </Typography>
+            <Typography variant="body2" className="campo-valor">
+              *********
+            </Typography>
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleLogout}
+            className="logout-button"
+          >
+            Cerrar sesión
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 };
